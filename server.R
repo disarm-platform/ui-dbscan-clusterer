3863cd65956164f2b672c5fe0fd827461af80a3e
Hugh Sturrock <hughsturrock@gmail.com>
2019-03-28 09:47:58 -0700

    Updates to server

--- server.R
+++ server.R
@@ -18,18 +18,19 @@ library(base64enc)
 
 
 # Define map
-map <- leaflet(max) %>%
-  addTiles(
-    "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}.png"
+map <- leaflet() %>%
+  addProviderTiles(
+    "CartoDB.Positron"
   )
 
 
 
 shinyServer(function(input, output) {
+  
   map_data <- eventReactive(input$goClusterYourself, {
     geo_in <- input$geo_file_input
     
-    
+    withProgress(message = 'Clustering points..',{
     # if (is.null(geo_in) | length(input$geo_text_input)==1)
     #   return(NULL)
     #
@@ -47,71 +48,85 @@ shinyServer(function(input, output) {
     } else{
       input_geo <- input$geo_text_input
     }
-    
+
     parcel_by <- input$parcel
+
     
     # PAckage up
     request_list <- list(
       subject = input_geo,
-      parcel_by = unlist(strsplit(parcel_by, ",")),
+      parcel_by = unlist(strsplit(stri_replace_all_regex(str=parcel_by, pattern=" ", repl=""), ",")),
       max_num = input$Max_Size,
       max_dist_m = input$buffer,
       return_type = input$return_type
     )
+
+    if(length(parcel_by)<=1){
+      request_list$parcel_by <- NULL
+    }
     
     response <-
       httr::POST(url = "https://faas.srv.disarm.io/function/fn-dbscan-clusterer",
                  body = as.json(request_list),
                  content_type_json())
-    return(response)
+
+    response_content <- content(response)
+    return(response_content$result)
+    })
     
   })
   
   
   
   output$pop_map <- renderLeaflet({
-    
-    if (is.null(map_data())) {
-      return(map %>% setView(0, 0, zoom = 2))
+
+    if(input$goClusterYourself[1]==0){
+      return(map %>% setView(0,0,zoom=2))
     }
-    
+
+    # Get output
+    map_data <- map_data()
+    print("here 1")
     # Get response if there is one and define color palettes
     if (input$return_type == "subject" | input$return_type == "both") {
-      subject_points <- st_read(as.json(from(JSON(
-        map_data()$subject
-      ))))
+      subject_points <- st_read(as.json(
+        map_data$result$subject
+      ))
       cluster_pal <-
-        colorNumeric(brewer.pal(10, "Set3"), subject_points$cluster_id)
+        colorNumeric(brewer.pal(10, "Set3"), as.numeric(subject_points$cluster_id))
     }
-    
+    print("here 2")
+
     if (input$return_type == "hull" | input$return_type == "both") {
-      hull_polys <- st_read(as.json(from(JSON(
-        map_data()$hull
-      ))))
+      hull_polys <- st_read(as.json(
+        map_data$result$chull_polys
+      ))
     }
-    
+    print("here 3")
     # Map results
     if (input$return_type == "subject") {
-      map %>% addCircleMarkers(
+      return(map %>% addCircleMarkers(
         data = subject_points,
         radius = 1,
-        color = cluster_pal(subject_points$cluster_id)
+        color = cluster_pal(as.numeric(subject_points$cluster_id))
+      ))
+    }
+    print("here 4")
+    if (input$return_type == "hull") {
+      return(map %>% addPolygons(data = hull_polys,
+                          weight = 3)
       )
     }
-    
-    if (input$return_type == "hull") {
-      map %>% addPolygons(data = hull_polys,
-                          weight = 3)
-    }
-    
+    print("here 5")
     if (input$return_type == "both") {
-      map %>% addCircleMarkers(
+      return(map %>% addCircleMarkers(
         data = subject_points,
         radius = 1,
-        color = cluster_pal(subject_points$cluster_id)
+        color = cluster_pal(as.numeric(subject_points$cluster_id))
       ) %>%
         addPolygons(data = hull_polys,
                     weight = 3)
+      )
     }
     
   })
